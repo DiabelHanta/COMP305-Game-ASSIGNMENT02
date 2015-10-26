c18e2bf8e73ab46abb73268364c2631328154c70
DiabelHanta <faim.khandker@gmail.com>
2015-10-25 21:36:46 -0400

    Second Commit
    second commit failed. everything added here is the same from there

---
+++ Assets/_Scripts/DeathTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	//NOTE: on trigger reloads the level/game
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//
+		if (other.gameObject.CompareTag ("Player")) 
+		{
+			//reopens the scene
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//VELOCITY UTILITY CLASS
+public class VelocityRange
+{
+	//PUBLIC INSTANCE VARIABLES
+	public float vMin, vMax;
+
+	//CONSTRUCTOR
+	public VelocityRange(float vMin, float vMax)
+	{
+		this.vMin = vMin;
+		this.vMax = vMax;
+	}
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	//PUBLIC INSTANCE VARIABLES
+	public float speed = 50f;
+	public float jump = 500f;
+	public VelocityRange velocityRange = new VelocityRange(300f, 1000f); //restricting min and max for velocity range
+
+	//PRIVATE INSTANCE VARIABLES
+	private AudioSource[] _audioSources; //array of audio sources
+	private AudioSource _elfSound; //pickup item sound
+	private AudioSource _jumpSound; 
+	private Rigidbody2D _rb2d;
+	private Transform _transform;
+	private Animator _anim;
+
+	//
+	private float _movingValue = 0f;
+	private bool _isFacingRight = true;
+	private bool _isGrounded = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Object references
+		this._rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		this._transform = gameObject.GetComponent<Transform> ();
+		this._anim = gameObject.GetComponent<Animator> ();
+
+//		this._audioSources = gameObject.GetComponent<AudioSource>();
+		//audio references
+//		this._elfSound = this._audioSources [0];
+//		this._elfSound = this._audioSources [1];
+	}
+
+	// Physics update
+	void FixedUpdate () 
+	{
+		//"forced" positions
+		float forceX = 0f;
+		float forceY = 0f;
+
+		//Absolute values of rigidbody's velocity
+		float absVelocityX = Mathf.Abs (this._rb2d.velocity.x);
+		float absVelocityY = Mathf.Abs (this._rb2d.velocity.y);
+
+		//Horizontal movement for player
+		this._movingValue = Input.GetAxis ("Horizontal");
+
+		//CONDITIONAL - give player "movement"
+		if (_movingValue != 0) 
+		{
+			//checks if player is grounded
+			if (this._isGrounded) 
+			{
+				this._anim.SetInteger ("AnimState", 1);
+
+				//neseted CONDITIONAL - moves player right
+				if (this._movingValue > 0) 
+				{
+					//moves right
+					if (absVelocityX < this.velocityRange.vMax) 
+					{
+						forceX = this.speed;
+						this._isFacingRight = true;
+						this._flip ();
+					}
+				} 
+
+				else if (this._movingValue < 0) 
+				{
+					//moves left
+					if (absVelocityX < this.velocityRange.vMax) {
+						forceX = -this.speed;
+						this._isFacingRight = false;
+						this._flip ();
+					}
+				}
+			}
+		}
+		//
+		else
+		{
+			//set idle animation here
+			this._anim.SetInteger("AnimState", 0);
+		}
+	
+		//checks if player is jumping (using ARROW-UP/'W')
+		//			//conditional for input button for "jump" (NOTE: FROM BASIC TUTORIAL)
+		//			if (Input.GetButtonDown ("Jump") && _grounded) 
+		//			{
+		//				// boolean used to ensure when player is jumping
+		//				jump = true;
+	
+		//checks if player is jumping
+		if((Input.GetKey ("up") || Input.GetKey (KeyCode.W)))
+		{
+			//checks if player is grounded
+			if(this._isGrounded)
+			{
+				this._anim.SetInteger("AnimState", 2);
+				if(absVelocityY < this.velocityRange.vMax)
+				{
+					forceY = this.jump;
+//					this._jumpSound.Play();
+					this._isGrounded = false;
+				}
+			}
+		}
+		//adds force for player to move him
+		this._rb2d.AddForce(new Vector2(forceX, forceY));
+	}
+
+	//INSERT AUDIO FILE!
+	//COLLISION METHODS
+//	void OnCollisionEnter2D(Collider2D other)
+//	{
+//		//player hits coin then play "_elfSound"
+//		if(other.gameObject.CompareTag("Coin"))
+//		{
+//			this._elfSound.Play ();
+//		}
+//	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+		//player hits/touches platform then "isGrounded" is set to true
+		//NOTE: allows player to hit "jump"
+		if(other.gameObject.CompareTag("Platform"))
+		{
+			this._isGrounded = true;
+		}
+	}
+
+	//PRIVATE METHODS
+	//flipping sprite's direction
+	private void _flip()
+	{
+		if(this._isFacingRight)
+		{
+			//changes scale of the sprite
+			this._transform.localScale = new Vector3(1f, 1f, 1f); //resets to normal scale
+		}
+		else
+		{
+			this._transform.localScale = new Vector3(-1f, 1f, 1f); //changes the scale to opposite
+		}
+	}
+
+}
